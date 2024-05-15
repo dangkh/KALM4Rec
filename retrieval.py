30cec09d05940998d5fcf596956fda817f1e04a8
dangkh <dangkh109@gmail.com>
2024-05-16 04:27:55 +0700

    fix bug in main

--- retrieval.py
+++ retrieval.py
@@ -1,4 +1,5 @@
-import world
+import argparse
+from ast import parse
 import os
 import time
 from retrievalHelper.utils import *
@@ -9,98 +10,137 @@ from retrievalHelper.models import *
 GPU = torch.cuda.is_available()
 device = torch.device('cuda' if GPU else "cpu")
 
-# from retrievalHelper.CBR import CBR
+random_seed = 1001
+setSeed(random_seed)
 
+listcity = ['charlotte', 'edinburgh', 'lasvegas', 'london', 'phoenix', 'pittsburgh', 'singapore']
 
-print("Running retrieval... ")
+parser = argparse.ArgumentParser()
+parser.add_argument('--city', type=str, default='edinburgh', help=f'choose city{listcity}')
+parser.add_argument('--quantity', type=int, default=20, help='number of keyword retrieval')
+parser.add_argument('--seed', type=int, default=1001, help='number of keyword retrieval')
+parser.add_argument('--edgeType', type=str, default='IUF', help='future work, current using TF-IDF')
+
+'''
+Export args
+'''
+parser.add_argument('--logResult', type=str, default='./log', help='write log result detail')
+parser.add_argument('--export2LLMs', action='store_true', help='whether export list of data for LLMs or not. \
+                                                                default = False')
+
+'''
+Model args
+'''
+parser.add_argument('--RetModel', type=str, default='MPG_old', help='jaccard, MF, MVAE, CBR, MPG_old, MPG')
+parser.add_argument('--numKW4FT', type=int, default=20, help='number of keyword for feature')
+
+'''
+CBR args
+'''
+parser.add_argument('--hidden_dim', type=int, default=32, help='hidden_dim')
+parser.add_argument('--lr', type=float, default=0.03, help='learning_rate')
+parser.add_argument('--num_epochs', type=int, default=500, help='num_epochs')
+parser.add_argument('--batch_size', type=int, default=128, help='batch_size')
+
+args = parser.parse_args()
+
+print("args:", args)
 print("Starting time: ", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-print("INFO: ", world.config)
-
-city = world.config['city']
 print("Loading training keyword")
-trainDat, testDat = data_reviewLoader(city)
-
+trainDat, testDat = data_reviewLoader(args.city)
 train_users, train_users2kw = extract_users(trainDat['np2users'])
 test_users, test_users2kw = extract_users(testDat['np2users'])
 
 # extract user2rest for label
-groundtruth = load_groundTruth(f'./data/reviews/{city}.csv')
+gt = load_groundTruth(f'./data/reviews/{args.city}.csv')
+# load edgeType
 
-keywordScore, keywordFrequence = load_kwScore(city, world.config["edgeType"])
 
-
+keywordScore, keywordFrequence = load_kwScore(args.city, args.edgeType)
 restGraph = retaurantReviewG([trainDat, keywordScore, keywordFrequence, \
-                                world.config["quantity"], world.config["edgeType"], groundtruth])
+                                args.quantity,  args.edgeType, gt])
 
-KNN = neighbor4kw(f'{city}_kwSenEB_pad', testDat,  restGraph)
+KNN = neighbor4kw(f'{args.city}_kwSenEB_pad', testDat,  restGraph)
 rest_Label = getRestLB(trainDat['np2rests'])
+sourceFile = open(args.logResult, 'a')
 
-sourceFile = open(world.config["logResult"], 'a')
+
 print('*'*10, 'Result' ,'*'*10, file = sourceFile)
-mp, mr, mf = 0, 0, 0
 dictionary = {}
-if world.config["RetModel"] == "CBR":
-	userFT, userFT_test, rest_feature = KNN.loadFT(world.config["numKW4FT"], rest_Label, world.config["city"])
-	label_train, label_test = label_ftColab(train_users, test_users, groundtruth, restGraph.numRest, rest_Label)
-	dim_users, dim_items = 384, 384
-    learning_rate = world.modelConfig["learning_rate"]
-    hidden_dim = world.modelConfig['hidden_dim']
-    num_epochs = world.modelConfig['num_epochs']
 
-	trainLB = np.asarray(label_train)
-	testLB = np.asarray(label_test)
-	print("feature shape (train / test):")
+if args.RetModel == "CBR":
+    print("*"*50)
+    print("Using CBR")
+    print("*"*50)
+
+    userFT, userFT_test, rest_feature = KNN.loadFT(args.numKW4FT, rest_Label, args.city)
+    label_train, label_test = label_ftColab(train_users, test_users, gt, restGraph.numRest, rest_Label)
+
+
+    dim_users, dim_items = 384, 384
+    learning_rate = args.lr
+    hidden_dim = args.hidden_dim
+    num_epochs = args.num_epochs
+
+    trainLB = np.asarray(label_train)
+    testLB = np.asarray(label_test)
+
+    print("feature shape (train / test):")
     print(userFT.shape, userFT_test.shape)
     train_dataset = DataCF(userFT, trainLB)
     test_dataset = DataCF(userFT_test, testLB)
     model = MatrixFactorization(dim_users, dim_items, hidden_dim).to(device)
-    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
-	test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
-	rest_feature = torch.from_numpy(rest_feature).type(torch.FloatTensor)
-	rest_feature = rest_feature.to(device)
 
-	
-	print(model)
-	optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay= 1e-4)
-	criterion = nn.BCELoss()
-	# Training loop
-	for epoch in range(num_epochs):
-	    total_loss = 0.0
-	    model.train()
-	    for batch_idx, batchDat in enumerate(train_loader):
-	        optimizer.zero_grad()
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+    rest_feature = torch.from_numpy(rest_feature).type(torch.FloatTensor)
+    rest_feature = rest_feature.to(device)
+
+    # Model, optimizer, and loss function
+
+    mp, mr, mf = 0, 0, 0
+    print(model)
+    # stop
+    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay= 1e-4)
+    criterion = nn.BCELoss()
+    criterion2 = nn.MSELoss()
+    # Training loop
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        model.train()
+        for batch_idx, batchDat in enumerate(train_loader):
+            optimizer.zero_grad()
             data, label = batchDat
             userDat = data.to(device)
             label = label.to(device)
             restDat = rest_feature
             prediction = model(userDat, restDat)
             loss = criterion(prediction , label)
-        loss.backward()
-        total_loss += loss.item() 
-        optimizer.step()
 
-    	print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss}')
+            # Backward pass and optimization step
+            loss.backward()
+            total_loss += loss.item() 
+            optimizer.step()
 
-    	if (epoch+1) % 10 == 0:
-        
-	        lResults = evaluateModel(model, test_loader, rest_feature, groundtruth, test_users, world.config['quantity'], rest_Label)
+            # Print training progress
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {total_loss}')
 
-	        p, r, f = extractResult(lResults)
-	        if mean(r) > mean(mr):
-	            mp, mr, mf = p, r, f
-	        print(f'Epoch [{epoch+1}/{num_epochs}], prec: {mean(p)}, rec: {mean(r)}, f1: {mean(f)}')
-	        
+        if (epoch+1) % 10 == 0:
+            
+            lResults = evaluateModel(model, test_loader, rest_feature, gt, test_users, args.quantity, rest_Label)
+            p, r, f = extractResult(lResults)
+            if mean(r) > mean(mr):
+                mp, mr, mf = p, r, f
+            print(f'Epoch [{epoch+1}/{num_epochs}], prec: {mean(p)}, rec: {mean(r)}, f1: {mean(f)}')
+            
 
-if world.config["export2LLMs"]:
+if args.export2LLMs:
     json_object = json.dumps(dictionary, indent=4)
-    with open(f"{city}_knn2rest_{world.config['RetModel']}.json", "w") as outfile:
+    with open(f"{args.city}_knn2rest_CBR.json", "w") as outfile:
         outfile.write(json_object)
-
 p, r, f = mp, mr, mf 
-print("args:", world.config, file = sourceFile)
+print("args:", args, file = sourceFile)
 print(mean(p), mean(r), mean(f), file = sourceFile)
 print('*'*10, 'End' ,'*'*10, file = sourceFile)
 sourceFile.close()
 print("End time: ", time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-
-
